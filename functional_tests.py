8de724e80b95b2801d77aaec155109ceb90fadeb
s-q-n <sqn@protonmail.com>
2018-06-06 15:08:55 +0200

    Added few things to front page

--- functional_tests.py
+++ functional_tests.py
@@ -42,7 +42,8 @@ class NewVisitorTest(unittest.TestCase):
             table = self.browser.find_element_by_id('id_list_table')
             rows = table.find_elements_by_tag_name('tr')
             self.assertTrue(
-                any(row.text == '1: Drink Margarittas' for row in rows)
+                any(row.text == '1: Drink Margarittas' for row in rows),
+                "New to-do item not appear in table"
             )
 
             # You still see the box inviting to add another item, You enter
