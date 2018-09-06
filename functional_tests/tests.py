4f38b37dfe0c182b3af02877a80815cdc0f9c901
s-q-n <sqn@protonmail.com>
2018-09-06 14:59:56 +0200

    Imported functional tests into django using LiveServerTestCase

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,11 +1,11 @@
+from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 import time
-import unittest
 
 
-class NewVisitorTest(unittest.TestCase):
+class NewVisitorTest(LiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
@@ -22,7 +22,7 @@ class NewVisitorTest(unittest.TestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
 
         # To see the webpage you need to go to the homepage
-        self.browser.get('http://localhost:8000')
+        self.browser.get(self.live_server_url)
 
 
         # You can easly know you are in correct place by the title and header
@@ -73,5 +73,3 @@ class NewVisitorTest(unittest.TestCase):
 
         # You are happy, you quit your browser and go mix your drinks
 
-if __name__ == '__main__':
-    unittest.main(warnings='ignore')
